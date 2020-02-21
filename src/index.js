4abed1da532941fb3dc2c10793e806313b106871
tigraranaar <Tigran1402_3991>
2020-02-21 23:07:43 +0400

    feat: add array functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+
+  if (array.length <= 0) {
+    return 0;
+  }
+
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+
+  if (array.length <= 0) {
+    return 0;
+  }
+  
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+
+  if (array.length <= 0) {
+    return 0;
+  }
+
+  return array.reduce((sum, i) => sum + i, 0) / array.length;
 }
